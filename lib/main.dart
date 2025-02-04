cf493b36982b9bbec6223dc63f1534b6d5bd0d8c
blackcat417 <ssoo174@ewha.ac.kr>
2025-02-04 17:58:52 +0900

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: SplashScreen(),
+    ),
+  );
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WebViewApp()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // 배경색 설정
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          width: 300,
+        ), // 로고 이미지 추가
+      ),
+    );
+  }
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://shortkong.pages.dev'));
+  }
+
+  // 🔹 전 화면으로 이동하는 함수 (뒤로 가기 기능)
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return false; // 앱 종료 안 함
+    }
+    return true; // 앱 종료
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      // 🔹 뒤로 가기 감지
+      onWillPop: () => _goBack(context),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          top: true,
+          bottom: false,
+          child: WebViewWidget(controller: _controller),
+        ),
+      ),
+    );
+  }
+}
